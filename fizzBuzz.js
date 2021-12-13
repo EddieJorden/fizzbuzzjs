f77f95c20c7809c605bfee2c27e3111ec58480bc
EddieJorden <eddie@eddiejorden.tech>
2021-12-13 06:34:34 -0800

    some slightly better fizz buzz's ==ej==

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,6 +3,8 @@
 // whe its divisible by 5 print out "buzz"
 // and when its devisble by both 3 and 5 print out "fizz buzz"
 
+// first solution
+
 let numbersArray = []
 let fizzBuzzArray = []
 
@@ -23,7 +25,7 @@ const fizzBuzz = (num) => {
         }
         if((j % 5) === 0 && (j % 3) !== 0) {
             fizzBuzzArray.push('buzz')
-        // console.log(`${j} % 5 === 0: buzz`)
+        // console.log(`${j} % 5 === 0: buzz`)`
         } else if ((j % 3) !== 0 && (j % 5) !== 0) {
             fizzBuzzArray.push(j)
             // console.log(`else ${j}`)
@@ -32,3 +34,29 @@ const fizzBuzz = (num) => {
     return fizzBuzzArray
 }
 console.log(fizzBuzz(100))
+
+// solution two
+const fizzBuzzArrayTwo = []
+const fizzBuzzTwo = (num) => {
+    for(let i = 1; i < num; i++) {
+        if(i % 15 === 0) {
+            fizzBuzzArrayTwo.push('fizz buzz')
+        } else if(i % 5 === 0) {
+            fizzBuzzArrayTwo.push('buzz')
+        } else if(i % 3 === 0) {
+            fizzBuzzArrayTwo.push('fizz')
+        }else fizzBuzzArrayTwo.push(i)
+    }
+    return fizzBuzzArrayTwo
+}
+
+console.log(fizzBuzzTwo(100))
+
+// solution three
+const fizzBuzzArrayThree = []
+const fizzBuzzThree = (num) => {
+    for (let i = 1; i < 101;) console.log((i++ % 3 ? '' : 'fizz') + (i % 5 ? '' : 'buzz') || i)
+    return fizzBuzzArrayThree
+}
+
+console.log(fizzBuzzThree(100))
